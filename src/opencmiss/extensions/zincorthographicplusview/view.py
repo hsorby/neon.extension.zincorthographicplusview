e1e31c1bdf0dd9b81e8ddc0ad860a61e520c2f89
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-08-30 09:37:44 +1200

    Implement full orthographic code. Emit signal when all sceneviewers are ready.

--- src/opencmiss/extensions/zincorthographicplusview/view.py
+++ src/opencmiss/extensions/zincorthographicplusview/view.py
@@ -1,11 +1,15 @@
 
+from PySide2.QtCore import Signal
 
 from opencmiss.neon.extensions.zinc_views import ZincOrthographicPlusView as BaseZincView
+from opencmiss.zincwidgets.sceneviewerwidget import ProjectionMode
 
 from opencmiss.extensions.zincorthographicplusview.ui_orthographicpluswidget import Ui_OrthographicPlusWidget
 
 
 class ZincOrthographicPlusView(BaseZincView):
+
+    graphics_initialized = Signal()
 
     def __init__(self, context, shared_opengl_widget, parent=None):
         super(ZincOrthographicPlusView, self).__init__(parent)
@@ -13,9 +17,13 @@ class ZincOrthographicPlusView(BaseZincView):
         self._ui.setupUi(self, shared_opengl_widget)
         self._context = context
         self._ui.xOrthographicView_widget.setContext(context)
+        self._ui.xOrthographicView_widget.setContext(context)
         self._ui.yOrthographicView_widget.setContext(context)
         self._ui.zOrthographicView_widget.setContext(context)
         self._ui.full3DView_widget.setContext(context)
+        self._view_list = [self._ui.xOrthographicView_widget, self._ui.yOrthographicView_widget,
+                           self._ui.zOrthographicView_widget, self._ui.full3DView_widget]
+        self._view_graphics_initialized = [False, False, False, False]
         self._orthographic_rev_normal_map = {self._ui.xOrthographicView_widget: [-1, 0, 0],
                                       self._ui.yOrthographicView_widget: [0, -1, 0],
                                       self._ui.zOrthographicView_widget: [0, 0, -1]}
@@ -37,15 +45,25 @@ class ZincOrthographicPlusView(BaseZincView):
         self._ui.xOrthographicView_widget.graphicsInitialized.connect(self._graphics_initialized)
         self._ui.yOrthographicView_widget.graphicsInitialized.connect(self._graphics_initialized)
         self._ui.zOrthographicView_widget.graphicsInitialized.connect(self._graphics_initialized)
+        self._ui.full3DView_widget.graphicsInitialized.connect(self._graphics_initialized)
 
     def _graphics_initialized(self):
         sender = self.sender()
+        index = self._view_list.index(sender)
+        self._view_graphics_initialized[index] = True
+        # sender._sceneviewer.setPerturbLinesFlag(True)
         if sender in [self._ui.xOrthographicView_widget,
                       self._ui.yOrthographicView_widget,
                       self._ui.zOrthographicView_widget]:
             sender.setTumbleRate(0)
+            # sender.setProjectionMode(ProjectionMode.PARALLEL)
             eye, look_at, up, angle = sender.getViewParameters()
             rev_n = self._orthographic_rev_normal_map[sender]
             eye = [look_at[0] - rev_n[0], look_at[1] - rev_n[1], look_at[2] - rev_n[2]]
+            if sender == self._ui.yOrthographicView_widget:
+                up = [0.0, 0.0, -1.0]
             sender.setViewParameters(eye, look_at, up, angle)
             sender.viewAll()
+
+        if all(self._view_graphics_initialized):
+            self.graphics_initialized.emit()
